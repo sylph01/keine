13878854e0b628132b2607c6ab0e3fbe838df10c
sylph01 <sylph01@s01.ninja>
2020-09-13 02:43:31 +0000

    disable starttls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'localhost'
+    address: 'localhost',
+    enable_starttls_auto: false
   }
 end
