f9e002a9b62f6767183ca604061319e11d78f44b
Ryo Kajiwara <sylph01@s01.ninja>
2020-09-13 16:39:30 +0900

    should check local part of "to" not "from"

--- app/mailboxes/registration_mailbox.rb
+++ app/mailboxes/registration_mailbox.rb
@@ -1,8 +1,8 @@
 class RegistrationMailbox < ApplicationMailbox
   def process
-    from_address = mail.from.first
-    local_part   = from_address.split("@")[0]
-    user = User.find_by(uuid: local_part)
+    from_address     = mail.from.first
+    local_part_of_to = mail.to.first.split("@").first
+    user = User.find_by(uuid: local_part_of_to)
     if user != nil
       if user.redeemed? == false
         user.email = from_address
