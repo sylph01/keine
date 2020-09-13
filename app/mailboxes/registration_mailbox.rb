55563e02b87160ac451c002a6697950c30d4f939
Ryo Kajiwara <sylph01@s01.ninja>
2020-09-13 11:24:47 +0900

    Bounce with bounce mailer

--- app/mailboxes/registration_mailbox.rb
+++ app/mailboxes/registration_mailbox.rb
@@ -1,7 +1,8 @@
 class RegistrationMailbox < ApplicationMailbox
   def process
     if mail.subject == "hogea"
-      inbound_email.bounced!
+      # inbound_email.bounced!
+      bounce_with BounceMailer.bounce(email: inbound_email.mail['from'])
     else
     end
   end
