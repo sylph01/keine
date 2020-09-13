965673f5283632521ebdff1637c6452f922978dd
Ryo Kajiwara <sylph01@s01.ninja>
2020-09-13 16:24:57 +0900

    mail.from contains a list of addresses

--- app/mailers/greet_mailer.rb
+++ app/mailers/greet_mailer.rb
@@ -2,7 +2,7 @@ class GreetMailer < ApplicationMailer
   default from: 'greet@kaigionrails.s01.ninja'
 
   def greet(inbound_email)
-    @from_address = inbound_email.mail['from']
+    @from_address = inbound_email.mail.from[0]
     mail(to: @from_address, subject: "Hello!")
   end
 end
