e6259cd2ed3be5cdb045bafaabe53a1ef217590f
sylph01 <sylph01@s01.ninja>
2020-09-13 02:05:43 +0000

    Modifications to run on server

--- forward.sh
+++ forward.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 HOME=/home/sylph01
+PATH=/usr/local/bin:$PATH
 
 cd /home/sylph01/keine && bin/rails action_mailbox:ingress:postfix URL="http://localhost:3000/rails/action_mailbox/relay/inbound_emails" INGRESS_PASSWORD=burfUdjetAfoquocwenOmhynadaihylgOd6fefyeavquienUj5
